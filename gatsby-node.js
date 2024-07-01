26d88c79eee81a2067e4f40eb28f2e58c5468735
tamaschreurs <tama.schreurs@gmail.com>
2024-07-02 00:20:47 +0200

    3rd window fix attempt

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       module: {
         rules: [
           {
-            test: /three-conic-polygon-geometry/,
+            test: /globe\.gl/,
             use: loaders.null(),
           },
         ],
