31e052214dec8e3b5b7038df51c0057b6ff75af6
tamaschreurs <tama.schreurs@gmail.com>
2024-07-02 22:53:27 +0200

    loading spinner

--- src/components/Globe.jsx
+++ src/components/Globe.jsx
@@ -59,7 +59,26 @@ function Globe() {
   // }
 
   if (typeof window === 'undefined') {
-    return <div>Windowless!</div>;
+    return (
+      <div className="h-screen flex justify-center bg-slate-100">
+        <svg
+          width="100"
+          height="100"
+          viewBox="0 0 50 50"
+          className="animate-spin m-auto"
+        >
+          <circle
+            cx="25"
+            cy="25"
+            r="20"
+            fill="none"
+            stroke="#94a3b8"
+            stroke-width="4"
+            stroke-dasharray="31.4 31.4"
+          />
+        </svg>
+      </div>
+    );
   }
 
   console.log('We found a window');
